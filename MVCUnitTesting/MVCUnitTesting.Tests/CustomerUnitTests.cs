06b332d05ac19ddd01c11fb655c78c4a42d69b7d
Pedro R. Jiménez <countzyx@gmail.com>
2013-09-16 02:16:16 -0400

    First Unit Test - CustomerUnitTests.cs
    This is the first testing class. Turns out it's already bad. Something about
    the ActionResult type being returned being something that won't work without
    some effort. Rather than do that, he wants the return type changed. This
    checkin also downloads the NuGet package for NUnit.

---
+++ MVCUnitTesting/MVCUnitTesting.Tests/CustomerUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+using NUnit.Framework;
+using MVCUnitTesting.Controllers;
+
+namespace MVCUnitTesting.Tests {
+    [TestFixture]
+    public class CustomerUnitTests {
+        [Test]
+        public void Customer_Index_Returns_ActionResult() {
+            // Arrange
+            CustomerController oController = new CustomerController();
+
+            // Act
+            var oActual = oController.Index();
+
+            // Assert
+            Assert.IsInstanceOf<ActionResult>(oActual);
+        }
+    }
+}
