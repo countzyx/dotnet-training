6ff2e40c02f8f25a013e0c28fe21cf7ff5691f63
Pedro R. Jiménez <countzyx@gmail.com>
2013-09-21 00:15:54 -0400

    Completed MVC 4 Fundamentals Ex 5 - Create a View That Uses StoreIndexViewModel
    Created a viewmodel and applied it to the Index action and view for the
    StoreController.

--- MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -4,30 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MvcMusicStore.ViewModels;
+
 namespace MvcMusicStore.Controllers
 {
-    public class StoreController : Controller
-    {
-        // GET: /Store/
-        public string Index()
-        {
-            return "Hello from Store.Index()";
-        }
+	public class StoreController : Controller
+	{
+		// GET: /Store/
+		public ActionResult Index()
+		{
+			var sGenres = new List<string> { "Country", "Disco", "Jazz", "Pop", "Rock" };
+			var oViewModel = new StoreIndexViewModel
+			{
+				NumberOfGenres = sGenres.Count(),
+				Genres = sGenres
+			};
 
-        // GET: /Store/Browse?genre=Disco
-        public string Browse(string genre)
-        {
-            string sMessage = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
-            
-            return sMessage;
-        }
+			return this.View(oViewModel);
+		}
 
-        // GET: /Store/Details/5
-        public string Details(int id)
-        {
-            string sMessage = "Store.Details, ID = " + id;
+		// GET: /Store/Browse?genre=Disco
+		public string Browse(string genre)
+		{
+			string sMessage = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+			
+			return sMessage;
+		}
 
-            return sMessage;
-        }
-    }
+		// GET: /Store/Details/5
+		public string Details(int id)
+		{
+			string sMessage = "Store.Details, ID = " + id;
+
+			return sMessage;
+		}
+	}
 }
