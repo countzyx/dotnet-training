ce80bddd9813281595262cd7a3130365dfde14f9
Pedro R. Jiménez <countzyx@gmail.com>
2013-10-05 04:40:55 -0400

    Completed Chapter 2 Guestbook
    Added a Guestbook section with its own controller. The controller has a simple
    Index action to list guestbook entries. It also has a Create action.
    
    The chapter is structured a little weird. There is no indication that the app
    should be run after you make changes at any point, so as you work through it
    you'll find yourself deleting things you had just created a little while ago
    for no reason. For example, in the Create POST action, we first return content
    stating that it succeeded. Soon after that we replace it with a redirect to
    the Index action. If we had executed the code to see the content return work
    first then it would have meant something. Anyway, the lesson is we need to
    build and execute after every change.

---
+++ Guestbook/Guestbook/Models/GuestbookContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Guestbook.Models {
+	public class GuestbookContext : DbContext {
+		public GuestbookContext() : base("Guestbook") { }
+
+		public DbSet<GuestbookEntry> Entries { get; set; }
+	}
+}
